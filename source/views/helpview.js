e2e74b9ca247c8d5aaed8ddd330c9a1f1fc2a464
Brent Hunter <aressel@gmail.com>
2014-07-30 12:45:44 -0700

    Updated metadata for 1.0.1

--- source/views/helpview.js
+++ source/views/helpview.js
@@ -11,33 +11,41 @@ enyo.kind({
 		]},
 		{kind: "enyo.Scroller", fit: true, style: "padding: 10px", components: [
 			{tag: "div", content: "FeedSpider 2", style: "font-size: 24px; font-weight: bold"},
-			{tag: "div", content: "Version 1.0.0 by Othello Ventures Ltd."},
+			{tag: "div", content: "Version 1.0.1 by Othello Ventures Ltd."},
 			{tag: "br"},
 			{tag: "div", components: [
 				{tag: "div", content: "FeedSpider 2 is a cross-platform port of the original FeedSpider RSS Reader for webOS, designed to work with Google Reader compatible applications."},
 				{tag: "br"},
 				{tag: "div", content: "It currently supports:"},
 				{tag: "ul", components: [
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://reader.aol.com"}, content: "AOL Reader" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://bazqux.com"}, content: "BazQux Reader" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://feedly.com"}, content: "Feedly" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://www.inoreader.com"}, content: "InoReader" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://www.owncloud.org"}, content: "OwnCloud News" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://tt-rss.org"}, content: "Tiny Tiny RSS" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://www.theoldreader.com"}, content: "The Old Reader" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://reader.aol.com", "target": "_blank"}, content: "AOL Reader" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://bazqux.com", "target": "_blank"}, content: "BazQux Reader" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://feedly.com", "target": "_blank"}, content: "Feedly" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://www.inoreader.com", "target": "_blank"}, content: "InoReader" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "https://www.owncloud.org", "target": "_blank"}, content: "OwnCloud News" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://tt-rss.org", "target": "_blank"}, content: "Tiny Tiny RSS" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://www.theoldreader.com", "target": "_blank"}, content: "The Old Reader" }]},
 				]}
 			]},
 			{tag: "br"},
 			{tag: "div", components: [
 				{tag: "div", content: "Reach us through:"},
 				{tag: "ul", components: [
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://twitter.com/feedspiderapp"}, content: "twitter" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "mailto:feedspider@feedspider.net"}, content: "email" }]},
-					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://www.feedspider.net"}, content: "web" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://twitter.com/feedspiderapp", "target": "_blank"}, content: "twitter" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "mailto:feedspider@feedspider.net", "target": "_blank"}, content: "email" }]},
+					{tag: "li", components: [ {tag: "a", attributes: {"href": "http://www.feedspider.net", "target": "_blank"}, content: "web" }]},
 				]}
 			]},
 			{tag: "br"},
 			{tag: "div", content: "History", style: "font-size: 20px; font-weight: bold"},
+			{tag: "div", components: [
+				{tag: "div", content: "1.0.1", style: "font-weight: bold" },
+				{tag: "ul", components: [
+					{tag: "li", content: "Fixes for several rendering bugs that would cause content to display incorrectly."},
+					{tag: "li", content: "Fixed handling of links so that they properly open in new browser window."},
+				]}
+			]},
+			{tag: "br"},
 			{tag: "br"},
 			{tag: "div", components: [
 				{tag: "div", content: "1.0.0", style: "font-weight: bold" },
