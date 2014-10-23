a627d96f48cca0c0a0dccb12629c120387c7f2c3
Brent Hunter <aressel@gmail.com>
2014-10-23 11:04:21 -0700

    Updated Help Doc and version number to 2.0.3

--- source/views/helpview.js
+++ source/views/helpview.js
@@ -11,7 +11,7 @@ enyo.kind({
 		]},
 		{kind: "enyo.Scroller", fit: true, style: "padding: 10px", components: [
 			{tag: "div", content: "FeedSpider 2", style: "font-size: 24px; font-weight: bold"},
-			{tag: "div", content: "Version 2.0.2 by Othello Ventures Ltd."},
+			{tag: "div", content: "Version 2.0.3 by Othello Ventures Ltd."},
 			{tag: "br"},
 			{tag: "div", components: [
 				{tag: "div", content: "FeedSpider 2 is a cross-platform port of the original FeedSpider RSS Reader for webOS, designed to work with Google Reader compatible applications."},
@@ -38,6 +38,12 @@ enyo.kind({
 			]},
 			{tag: "br"},
 			{tag: "div", content: "History", style: "font-size: 20px; font-weight: bold"},
+			{tag: "div", components: [
+				{tag: "div", content: "2.0.3", style: "font-weight: bold" },
+				{tag: "ul", components: [
+					{tag: "li", content: "Added support for Instapaper on Firefox OS"},
+				]}
+			]},
 			{tag: "div", components: [
 				{tag: "div", content: "2.0.2", style: "font-weight: bold" },
 				{tag: "ul", components: [
