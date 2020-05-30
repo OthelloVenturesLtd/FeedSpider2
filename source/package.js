32a74000e58f028fe4c00e7297030a441e1d7c53
Brent Hunter <bh@othelloventures.com>
2020-05-29 22:07:30 -0700

    Add spotlight and moonstone imports

--- source/package.js
+++ source/package.js
@@ -4,12 +4,16 @@ enyo.depends(
 	// Onyx UI library
 	"$lib/onyx",	// To theme Onyx using Theme.less, change this line to $lib/onyx/source,
 	//"Theme.less",	// uncomment this line, and follow the steps described in Theme.less
+	// Moonstone UI library
+	"$lib/moonstone",
 	// localization library: https://github.com/minego/macaw-enyo/tree/master/lib/simplelang
 	"$lib/simplelang",
 	// webos-lib webOS compatibility library
 	"$lib/webos-lib",
 	// Notification theme library
 	"$lib/NotificationTheme",
+	// Focus support library for TV applications
+	"$lib/spotlight",
 	// CSS/LESS style files
 	"style",
 	// Libraries
